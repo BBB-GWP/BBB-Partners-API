7522315dbb7b0c18dcd6314ff648178c085f3941
Yousef Shawky <211150997+yousef-shawky-etg@users.noreply.github.com>
2026-01-15 19:54:02 +0200

    hotfix: Deactiving User

--- Src/BBB-ApplicationDashboard.Api/Controllers/SyncController.cs
+++ Src/BBB-ApplicationDashboard.Api/Controllers/SyncController.cs
@@ -2,13 +2,11 @@ using System.Security.Claims;
 using BBB_ApplicationDashboard.Application.DTOs.PaginatedDtos;
 using BBB_ApplicationDashboard.Application.DTOs.Sync;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BBB_ApplicationDashboard.Api.Controllers
 {
-    public class SyncController(ISyncService syncService, IUserService userService)
-        : CustomControllerBase
+    public class SyncController(ISyncService syncService) : CustomControllerBase
     {
         [Authorize(Policy = "Internal")]
         [HttpGet("failures/internal")]
